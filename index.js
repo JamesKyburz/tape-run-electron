d2869d5033a517c21a8e79f485b232af786a28f4
James Kyburz <james.kyburz@gmail.com>
2015-04-12 20:17:14 +0200

    resolve without which

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var path = require('path')
-var which = require('which').sync
 var spawn = require('win-spawn')
 var concat = require('concat-stream')
 
@@ -10,10 +9,14 @@ resolve('tape')
 
 function resolve(module) {
   try {
-    return which(module)
+    return require(process.cwd() + '/node_modules/' + module)
   } catch(e) {
-    console.error('Cannot find ' + module + ' from here, please install it from npm')
-    process.exit(1)
+    try {
+      return require(module)
+    } catch(e) {
+      console.error('Cannot find ' + module + ' from here, please install it from npm')
+      process.exit(1)
+    }
   }
 }
 
